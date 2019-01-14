868afda51198523fb65448fae5de9340029c2c07
Anssam Ghezala <ghezala.anssam@gmail.com>
2019-01-14 15:53:58 -0500

    delete png files

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import SearchBar from "./SearchBar.js";
 import Results from "./Results.js";
+import { ReactComponent as Icon } from "./search-solid.svg";
 
 class App extends Component {
   render() {
@@ -11,7 +12,7 @@ class App extends Component {
           <header className="Header">Toronto Waste Lookup</header>
           <SearchBar className="SearchBar" />
           <button className="btn Button">
-            <span className="Icon" />
+            <Icon className="Icon" />
           </button>
           <Results className="Results" />
           <div className="Favourites"> Favourites </div>
--- src/Result.js
+++ src/Result.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import star from "./greyStar.png";
+
 class Result extends Component {
   state = { name: "", favourite: false };
   handleClick = () => {
@@ -10,12 +10,6 @@ class Result extends Component {
   render() {
     return (
       <div className={this.props.children}>
-        <img
-          src={star}
-          alt="star"
-          className="Star"
-          onClick={this.handleClick}
-        />
         <a> Hi </a>
       </div>
     );
